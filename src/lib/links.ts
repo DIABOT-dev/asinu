ea1dc189bb242c8fc09417491a71eb408a545088
ok <ok>
2026-07-31 15:29:00 +0700

    Point privacy link to current public policy

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,7 +1,7 @@
 import { Linking } from 'react-native';
 
 export const TERMS_URL = 'https://asinu.vn/terms/';
-export const PRIVACY_URL = 'https://asinu.vn/privacy/';
+export const PRIVACY_URL = 'https://asinu.top/privacy/';
 export const DATA_DELETION_URL = 'https://asinu.vn/data-deletion/';
 export const SUPPORT_EMAIL = 'mailto:info@asinu.vn';
 export const DEMO_ACCOUNT_EMAIL = 'demo@asinu.vn';
