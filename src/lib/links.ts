ab95519182801f3f7793be5fb6e8c676613b4add
ok <ok>
2026-08-01 11:27:06 +0700

    Align demo account password

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -5,7 +5,7 @@ export const PRIVACY_URL = 'https://asinu.top/privacy/';
 export const DATA_DELETION_URL = 'https://asinu.vn/data-deletion/';
 export const SUPPORT_EMAIL = 'mailto:info@asinu.vn';
 export const DEMO_ACCOUNT_EMAIL = 'demo@asinu.vn';
-export const DEMO_ACCOUNT_PASSWORD = 'Demoasinu2026';
+export const DEMO_ACCOUNT_PASSWORD = '!Demoasinu2026';
 
 export const openExternal = async (url: string) => {
   await Linking.openURL(url);
